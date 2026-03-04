b27d087283b351355c9edbcfa5390e22ac1498fb
AmosZ149 <amos.mikhael@gmail.com>
2026-03-04 10:05:04 +0700

    feat: setup tailwind

---
+++ app/views/students/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Daftar Siswa</title>
+    <link rel="stylesheet" href="/css/output.css">
+
+<body>
+    <h1 class="text-red-900">Daftar Siswa</h1>
+    <P>Menampilkan daftar siswa</P>
+</body>
+
+</html>
--- public/index.php
+++ public/index.php
@@ -1,18 +1,14 @@
 <?PHP
 
-require_once './app/core/Router.php';
+require_once '../app/core/Router.php';
 
 use app\core\Router;
 
 $router = new Router();
 
-$router->add('GET', '/students', 'StudentController', 'function');
-$router->add('GET', '/students/create', 'StudentController', 'function');
-
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
 
 $router->run();
-{
-  
-}
-
 ?>
