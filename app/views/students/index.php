2611068c79a8ec83477212386429e4dc87fe68af
AmosZ149 <amos.mikhael@gmail.com>
2026-04-01 10:16:42 +0700

    wip: implement student index page

--- app/views/students/index.php
+++ app/views/students/index.php
@@ -7,13 +7,58 @@
     <title>Daftar Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
 
-<body>
-<header class="bg-blue-300 text-white">
-    <div class="flex justify-between">
-    <a href="/students">sistem-sekolah</a>
-    <a href="/students/create">+ Tambah Siswa</a>
+<body class="flex flex-col min-h-screen bg-gray-100">
+<header class="bg-blue-400 text-white shadow">
+    <div class="flex items-center justify-between container mx-auto py-4">
+    <a class="font-bold" href="/students">sistem-sekolah</a>
+    <a class="py-2 px-4 bg-white text-blue-500 rounded-lg" href="/students/create">+ Tambah Siswa</a>
     </div>
 </header>
+<main class="grow container mx-auto">
+    <div class="mt-8">
+    <div class="bg-white shadow p-4 rounded-lg">
+        <h1 class="font-bold text-2xl">
+            Daftar Siswa
+        </h1>
+        <p>
+            Menampilkan daftar siswa
+        </p>
+    </div>
+    <div class="bg-white rounded-lg shadow">
+        <table>
+            <thead class="bg-gray-200">
+                <tr>
+                    <th class="text-left py-3 px-4">No</th>
+                    <th class="text-left py-3 px-4">Nama</th>
+                    <th class="text-left py-3 px-4">Kelas</th>
+                    <th class="text-left py-3 px-4">NIS</th>
+                    <th class="text-left py-3 px-4">No Telpon</th>
+                    <th class="text-left py-3 px-4">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="py-3 px-4">1</td>
+                    <td class="py-3 px-4">Andi</td>
+                    <td class="py-3 px-4">XI TKJ 1</td>
+                    <td class="py-3 px-4">1234</td>
+                    <td class="py-3 px-4">0812 3456 7890</td>
+                    <td class="py-3 px-4">
+                        <div class="flex justify-center item-center">
+                            <a href="" class="text-green-500">Detail</a>
+                            <a href="" class="text-yellow-500">Edit</a>
+                            <a href="" class="text-red-500">Hapus</a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+</main>
+<footer class="bg-gray-600 text-white p-4 text-center">
+    2026 - SMK Kristen Immanuel Pontianak - Sistem Sekolah 
+</footer>
 </body>
 
 </html>
