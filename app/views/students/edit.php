dcd5e3e77d999bbe458c1a79827f3294c4a0220a
AmosZ149 <amos.mikhael@gmail.com>
2026-04-07 14:36:22 +0700

    feat: implement student detail and edit page

--- app/views/students/edit.php
+++ app/views/students/edit.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Siswa</title>
+    <title>Edit Siswa</title>
     <link rel="stylesheet" href="/css/output.css">
 
 <body class="flex flex-col min-h-screen bg-gray-100">
@@ -18,14 +18,14 @@
         <div class="mt-8 space-y-4">
             <div class="bg-white shadow p-4 rounded-lg">
                 <h1 class="font-bold text-2xl">
-                    Detail Siswa
+                    Edit Siswa
                 </h1>
                 <p>
-                    Menampilkan detail siswa yang terdaftar
+                    Melakukan perubahan data siswa yang terdaftar
                 </p>
             </div>
             <div class="bg-white rounded-lg shadow">
-                <div class="p-4">
+                <form action="" class="p-4">
                     <div class="mb-5">
                         <label for="name" class="font-bold block text-gray-700  mb-2">Nama</label>
                         <input type="text" id="name" name="name" placeholder="Masukkan nama"
@@ -33,7 +33,7 @@
                     </div>
                     <div class="mb-5">
                         <label for="class" class="block text-gray-700 font-bold mb-2">Kelas</label>
-                        <input type="text" id="class" name="class" placeholder="Masukkan kelas" readonly
+                        <input type="text" id="class" name="class" placeholder="Masukkan kelas"
                             class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
                     </div>
                     <div class="mb-5">
@@ -43,18 +43,19 @@
                     </div>
                     <div class="mb-5">
                         <label for="no_telpon" class="block text-gray-700 font-bold mb-2">No Telpon</label>
-                        <input type="text" id="no_telpon" name="no_telpon" placeholder="Masukkan no telpon" readonly
+                        <input type="text" id="no_telpon" name="no_telpon" placeholder="Masukkan no telpon"
                             class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500">
                     </div>
                     <div class="mb-6">
                         <label for="address" class="block text-gray-700 font-bold mb-2">Alamat</label>
-                        <textarea id="address" name="address" placeholder="Masukkan alamat" readonly
+                        <textarea id="address" name="address" placeholder="Masukkan alamat"
                             class="w-full border border-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring focus:border-blue-500"></textarea>
                     </div>
                     <div class="flex justify-end items-center gap-4">
                         <a href="/students" class="px-4 py-2 rounded-lg bg-gray-100 text-blue-500 hover:underline">Kembali</a>
+                        <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">Simpan</button>
                     </div>
-</div>
+                </form>
             </div>
         </div>
     </main>
